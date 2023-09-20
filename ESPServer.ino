83e0f5a9cac9a9320e00fba6adce262b81d2a335
arpitarattan <89466680+arpitarattan@users.noreply.github.com>
2023-09-20 09:43:54 -0400

    Updated code with serial transmission

--- ESPServer.ino
+++ ESPServer.ino
@@ -2,18 +2,14 @@
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <SoftwareSerial.h>
+using namespace std;
 
 const char* ssid = "Arpita";
 const char* password = "arpita12";
-
-SoftwareSerial mySerial(7, 6)
-
-//reaction time module--------
-const int BUTTON = 2;
-const int LED = 3;
-//----------------------------
+WiFiClient clientGet;
 AsyncWebServer server(80);
 
+SoftwareSerial nano(3,1); //RX, TX
 //WEBSITE CODE
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
@@ -35,61 +31,16 @@ const char index_html[] PROGMEM = R"rawliteral(
   </style>
 </head>
 <body>
-  <h2>Welcome to Analytics Dashboard</h2>
-  <script>
-window.onload = function () {
+  <h2>Welcome to Analytics Dashboard</h2><br><br>
 
-var dps = []; // dataPoints
-var chart = new CanvasJS.Chart("chartContainer", {
-	title :{
-		text: "Dynamic Data"
-	},
-	data: [{
-		type: "line",
-		dataPoints: dps
-	}]
-});
-
-var xVal = 0;
-var yVal = 100; 
-var updateInterval = 1000;
-var dataLength = 20; // number of dataPoints visible at any point
-
-var updateChart = function (count) {
-
-	count = count || 1;
-
-	for (var j = 0; j < count; j++) {
-		yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-		dps.push({
-			x: xVal,
-			y: yVal
-		});
-		xVal++;
-	}
-
-	if (dps.length > dataLength) {
-		dps.shift();
-	}
-
-	chart.render();
-};
-
-updateChart(dataLength);
-setInterval(function(){updateChart()}, updateInterval);
-
-}
-</script>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
 </body>
 </html>
 )rawliteral";
 
 
 void setup(){
-  Serial.begin(115200);
-
+  Serial.begin(9600);
+  nano.begin(115200);
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
   Serial.println("Connecting to WiFi..");
@@ -110,29 +61,9 @@ void setup(){
 }
 
 void loop() {
-
+  float reactiontime = nano.read();
+  Serial.print(reactiontime);
+  Serial.print("\n");
+  clientGet.print(reactiontime);
 }
 
-float reactiontime(){
-  float time = 0;
-  Serial.print("Press button to START!");
-  Serial.print("\n");
-  while(digitalRead(BUTTON) == 0){} //delay until pressed
-  delay(100);
-
-  delay(3000);
-  delay(random(5000));
-  digitalWrite(redLED, HIGH);
-  time = millis();//start counting
-
-  while(digitalRead(BUTTON) == 0){} //delay until clicked
-  digitalWrite(redLED, LOW);
-
-  //prints time
-  Serial.println("Your time was");
-  Serial.print((millis() - time));
-  Serial.println(" milliseconds");
-  Serial.print("\n");
-  delay(1000);
-  return(time);
-}
