b0367a9bcf89d8cecad48a908dc416b14baedf48
arpitarattan <89466680+arpitarattan@users.noreply.github.com>
2023-09-28 23:18:15 -0400

    broken code question mark

--- ESPServer.ino
+++ ESPServer.ino
@@ -4,6 +4,7 @@
 #include <SoftwareSerial.h>
 using namespace std;
 
+char readbuffer [64];
 const char* ssid = "Arpita";
 const char* password = "arpita12";
 WiFiClient clientGet;
@@ -32,14 +33,29 @@ const char index_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
   <h2>Welcome to Analytics Dashboard</h2><br><br>
-
+  %GRAPHPLACEHOLDER%
 </body>
 </html>
 )rawliteral";
 
+String processor(String var){
+  //Serial.println(var);
+  if(var == "GRAPHPLACEHOLDER"){
+    String graph = "";
+    
+    int i = 0; //initilaize iterator
+    while(nano.available()){
+      readbuffer[i] = nano.read();
+      i++;
+    }
+    readbuffer[i+1] = '\0';
+    graph += "<h4>Reaction Time: " + String(readbuffer);
+    return graph;
+  }
+  return String();
+}
 
 void setup(){
-  Serial.begin(9600);
   nano.begin(115200);
   // Connect to Wi-Fi
   WiFi.begin(ssid, password);
@@ -51,19 +67,16 @@ void setup(){
   Serial.println(WiFi.localIP());
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html);
+    request->send_P(200, "text/html", index_html, processor);
   });
   // Send a GET request to <ESP_IP>/update?output=<inputMessage1>&state=<inputMessage2>
-  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
-    request->send(200, "text/plain", "OK");
-  });
+  // server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+  //   request->send(200, "text/plain", "OK");
+  // });
   server.begin(); // Start server
 }
 
 void loop() {
-  float reactiontime = nano.read();
-  Serial.print(reactiontime);
-  Serial.print("\n");
-  clientGet.print(reactiontime);
+  // float reactiontime = nano.read();
+  // clientGet.print(reactiontime);
 }
-
