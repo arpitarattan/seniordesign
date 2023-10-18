dcd083cca4e2795acc403d76ce84cc0af5328f36
arpitarattan <89466680+arpitarattan@users.noreply.github.com>
2023-10-18 11:41:39 -0400

    RTT - Working rand function, external interrupts, and timer
    Need to work on delay function

--- RTTv1.c
+++ RTTv1.c
@@ -22,18 +22,18 @@ void setnC(int32_t pin_num, int32_t val);
 int32_t readpinB(int32_t pin_num);
 int32_t readpinA(int32_t pin_num);
 void rtt();
+void start_timer();
+uint32_t end_timer();
 
 void mysleep(int time) {
     for(int n = 0; n < time; n++);
 }
 void EXTI0_1_IRQHandler() {
-    while(readpinB(1) == 0);
+    while(readpinB(1) == 0); //PB1 - VMT
     EXTI->PR |= EXTI_PR_PR1;
-
     //while(1) {
        // mysleep(10000);
     setnB(0,1);
-
     mysleep(1000000);
     //setnA(7,1);
     //rtt();
@@ -42,17 +42,16 @@ void EXTI0_1_IRQHandler() {
     setnB(0,0);
 }
 void EXTI2_3_IRQHandler() {
-    while(readpinA(3) == 0);
+    while(readpinA(3) == 0); //PA3 - RTT
     EXTI->PR |= EXTI_PR_PR3;
-    setnA(0,1); //turn on green led //pb0
-    while(1){}
-
-    mysleep(1000000);
+    setnA(0,1); //turn on green led //pa0
+    setnA(1,1); //turn on red led
+    start_timer();
+    rtt();
     EXTI->PR &= ~EXTI_PR_PR3;
-    setnA(0,0);
 }
 void EXTI4_15_IRQHandler() {
-    while(readpinA(6) == 0);
+    while(readpinA(6) == 0); //PA6 - SMT
     EXTI->PR |= EXTI_PR_PR6;
 
     setnA(1,1); //turn on yellow led //pc0
@@ -81,97 +80,122 @@ void init_exti(){
     EXTI->FTSR &= ~EXTI_FTSR_TR6; // disable Trigger on falling edge
 
     NVIC_EnableIRQ(EXTI0_1_IRQn);
-    NVIC_SetPriority(EXTI0_1_IRQn,1);
+    NVIC_SetPriority(EXTI0_1_IRQn,2);
     NVIC_EnableIRQ(EXTI2_3_IRQn);
-    NVIC_SetPriority(EXTI2_3_IRQn,1);
+    NVIC_SetPriority(EXTI2_3_IRQn,2);
     NVIC_EnableIRQ(EXTI4_15_IRQn);
-    NVIC_SetPriority(EXTI4_15_IRQn,1);
-
+    NVIC_SetPriority(EXTI4_15_IRQn,2);
+}
+void TIM2_IRQHandler(void) {
+    TIM2->SR &= ~TIM_SR_UIF;
 }
 void init_tim2() {
     RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+    //TIM2->DIER |= TIM_DIER_UIE; //enable the timer update interrupt
     TIM2->PSC = 48000 - 1;
     TIM2->ARR = 10000 - 1;
     TIM2->CR1 &= ~TIM_CR1_DIR;
 
+    //where to set priority
 }
-void start_timer(){
+
+void start_timer() {
     TIM2->CR1 |= TIM_CR1_CEN;
+    NVIC_EnableIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn,1);
 }
 uint32_t end_timer() {
     TIM2->CR1 &= ~TIM_CR1_CEN;
-    return TIM2->CNT;
+
+    uint32_t time = TIM2->CNT;
+    NVIC_DisableIRQ(TIM2_IRQn);
+    return time;
 }
 uint32_t standard_reaction_time_test() {
 
-    int num = rand();
 
-    while(readpinA(5) == 0); //user needs to press button to start game
-    setnA(6,0);
-    setnA(7,0);
-    setnA(6, 1);              //turn the red led light on
-    mysleep(5000000);             //a min amount of time
-   // mysleep(num);             //a random amount of time for the green led to turn on
-    //delay(5000);
-    setnA(7,1);                //turn the green led on
-    setnA(6,0);               //turn the red led off
+    //pa0 -green LED
+    //pa1 - red LEd
+    //pa2 - button
+    while(readpinA(2) == 0); //user needs to press button to start game //pa2
+    mysleep(1000000);
+    uint32_t new_seed = end_timer();
+    srand(new_seed);
+    //uint32_t num = 100000+ rand() % (9999999 - 100000);
+    uint32_t num = 1000+ rand() % (60000 - 1000);
+    mysleep(100000);
+
+    setnA(0,0);
+    setnA(1,0);
+
+    setnA(1,1);              //turn the red led light on
+
+   // mysleep(3000000);             //a min amount of time
+    mysleep(num*480);
+    setnA(0,1);                //turn the green led on
+    setnA(1,0);               //turn the red led off
     start_timer();
-    while(readpinA(5) == 0); //wait for user to press button
-    setnA(7,0);                //turn off green led
-    setnA(6,0);
+    while(readpinA(2) == 0); //wait for user to press button
+    setnA(0,0);                //turn off green led
+    setnA(1,0);
     uint32_t reaction_time = end_timer();
     mysleep(1000000);
-    while(readpinA(5) == 0){
-        setnA(7,0);                //turn off green led
-        setnA(6,0);
+    while(readpinA(2) == 0) {
+        setnA(0,0);                //turn off green led
+        setnA(1,0);
     }
     mysleep(1000000);            //wait
     return reaction_time;
+}
+uint32_t unit_test1() {
+    //press button start timer
+    //press button end timer
+    while(readpinA(2) == 0);
+    mysleep(1000000);
+    setnA(1,1);
+    start_timer();
+    while(readpinA(2) == 0);
+    setnA(1,0);
+    uint32_t time = end_timer();
+
+    return time;
 }
 void rtt()
 {
     while(1) {
         uint32_t reaction_time = standard_reaction_time_test();
+//        uint32_t time = unit_test1();
         mysleep(1000000);
     }
     for(;;);
 }
+
+
 void inita() {
-	//input PA0
-	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
+    //inputs PA3 (RTT select), PA6 (SMT select), PA2 (RTT button)
+    //outputs (PA0, PA1)
+    RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;
 
-	GPIOA -> MODER &= ~GPIO_MODER_MODER0;
-    GPIOA -> MODER |= GPIO_MODER_MODER0_0;
-
-//	GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR0;
-//	GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR0_1;
-
-	//input PA1
-	GPIOA -> MODER &= ~GPIO_MODER_MODER1;
-    GPIOA -> MODER |= GPIO_MODER_MODER1_0;
-
-//    GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR1;
-//    GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR1_1;
-
-    //input PA3
-    GPIOA -> MODER &= ~GPIO_MODER_MODER3;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER3; //pa3
     GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR3;
     GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR3_1;
 
-    //input PA5 - rtt button
-    //GPIOA -> MODER &= ~GPIO_MODER_MODER5;
-//    GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR5;
-//    GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR5_1;
-
-    //output PA6 - Red LED
-    GPIOA -> MODER &= ~GPIO_MODER_MODER6;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER6; //pa6
     GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR6;
     GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR6_1;
 
-    //output PA7 - Green LED
-    GPIOA -> MODER &= ~GPIO_MODER_MODER7;
-    GPIOA -> MODER |= GPIO_MODER_MODER7_0;
-    GPIOA -> MODER &= ~GPIO_MODER_MODER7_1;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER2; //pa2
+    GPIOA-> PUPDR  &= ~GPIO_PUPDR_PUPDR2;
+    GPIOA-> PUPDR |= GPIO_PUPDR_PUPDR2_1;
+
+    //outputs
+    GPIOA -> MODER &= ~GPIO_MODER_MODER0; //pa0
+    GPIOA -> MODER |= GPIO_MODER_MODER0_0;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER0_1;
+
+    GPIOA -> MODER &= ~GPIO_MODER_MODER1; //pa1
+    GPIOA -> MODER |= GPIO_MODER_MODER1_0;
+    GPIOA -> MODER &= ~GPIO_MODER_MODER1_1;
 }
 void initb() {
    RCC -> AHBENR |= RCC_AHBENR_GPIOBEN;
@@ -251,9 +275,10 @@ int main(void)
     initb();
     initc();
     init_exti();
-
+    init_tim2();
     while(1) {
         //setn(8,1);
+
     }
     for(;;);
 }
