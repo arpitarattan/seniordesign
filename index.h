c98ef9382e5b66cebf1e2b9e56e28316a366652f
arpitarattan <89466680+arpitarattan@users.noreply.github.com>
2023-10-19 19:24:36 -0400

    Testing all 3 tetsts

--- index.h
+++ index.h
@@ -5,30 +5,52 @@ const char MAIN_page[] PROGMEM = R"=====(
 
 <div id="demo">
 <h1>Welcome to PBU Analytics</h1>
-</div>
-
-<div>
+</div><div>
 	Reaction Time : <span id="RT">0</span><br>
-</div>
-
-<script>
-function sendData(time) {
+  Visual Memory Level : <span id="VT">0</span><br>
+  Sequence Memory Level : <span id="ST">0</span><br>
+</div><script>
+function sendData(data) {
   var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("RT").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("POST", "/index", true); // Specify just the endpoint URL
-  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  var params = "RT=" + time; // Create the POST data
+  if(data.length >= 5 && data.length <= 9){
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("VT").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("POST", "/index", true); // Specify just the endpoint URL
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    data = data % 10000;
+    var params = "VT=" + data; // Create the POST data
+  }
+  else if(data.length >= 10){
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("ST").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("POST", "/index", true); // Specify just the endpoint URL
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    data = data % 100000000;
+    var params = "ST=" + data; // Create the POST data
+  }
+  else{
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("RT").innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("POST", "/index", true); // Specify just the endpoint URL
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    var params = "RT=" + data; // Create the POST data
+  }
   xhttp.send(params); // Send the POST request with the data
 }
 
 setInterval(function() {
   // Call a function repetatively with 2 Second interval
   sendData();
-}, 5000); //2000mSeconds update rate
+}, 2000); //2000mSeconds update rate
 
 </script>
 </body>
