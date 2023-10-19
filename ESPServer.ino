2777c82a00a8e9f6036057f4a81c047c9fd4c613
arpitarattan <89466680+arpitarattan@users.noreply.github.com>
2023-10-19 19:24:06 -0400

    Testing all 3 test data

--- ESPServer.ino
+++ ESPServer.ino
@@ -1,4 +1,4 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h> // lalala
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 #include <SoftwareSerial.h>
@@ -18,14 +18,20 @@ void handleRoot() {
  server.send(200, "text/html", s); //Send web page
 }
 
-void handletime() {
-  float time = 0;
+void handledata() {
+ int data = 0;
  if(nano.available()){
-  time = nano.parseFloat();
-  Serial.print(time);
+  String test = nano.readStringUntil('\n'); //can use to check which test is being received
+  data = nano.parseInt();
+  if(test[0] == 'v'){
+    data = data * 10000; //encode 4 zeros for sequence
+  }
+  else if(test[0] == 's'){
+    data = data * 100000000; //encode 5 zeros for sequence
+  }
  }
- String stringtime = String(time);
- server.send(200, "text/plain", stringtime); //Send ADC value only to client ajax request
+ String stringdata = String(data);
+ server.send(200, "text/plain", stringdata); //Send ADC value only to client ajax request
 }
 
 //==============================================================
@@ -33,25 +39,18 @@ void handletime() {
 //==============================================================
 void setup(void){
   Serial.begin(115200);
-  nano.begin(115200);
+  nano.begin(9600);
   WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-
-  //If connection successful show IP address in serial monitor
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
   Serial.println(WiFi.localIP());  //IP address assigned to your ESP
  
   server.on("/", handleRoot);      //Which routine to handle at root location
-  server.on("/index", handletime);      //Which routine to handle at root location
+  server.on("/index", handledata);      //Which routine to handle at root location
 
   server.begin();                  //Start server
   Serial.println("HTTP server started");
